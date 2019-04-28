35b84bcfb869d6a9b7603316bbe94a88b66d5f7b
Nick Whyte <nick@nickwhyte.com>
2019-04-28 22:14:09 +1000

    Update dotfiles

--- zsh/canva-pro.sh
+++ zsh/canva-pro.sh
@@ -1,21 +1,10 @@
 unalias gr
 alias lgtms='git checkout master && ggpull && git checkout - && /Users/nickw/work/canva/tools/check-lgtms/check_lgtms.sh';
-alias grebase='git checkout green && ggpull && git checkout - && git rebase green';
 
 alias gll="git log --graph --oneline --decorate --all" # Show git log in a tree for all branches
 alias gl='git log --graph --oneline --decorate $(git branch | cut -c 3- | grep nwhyte) master' # Show only nwhyte branches + master + green
 alias gln='git log --oneline --graph --decorate $(git branch | cut -c 3- | grep nwhyte)' # Visualise all branches with nwhyte.
 
-
-drebase() {
-    if [ $# -ne 1 ]; then
-        echo "Usage: $0 <branch to rebase onto>";
-        return 1
-    fi;
-
-    echo ">" git rebase --onto "$1" "${1}@{1}";
-    git rebase --onto "$1" "${1}@{1}";
-}
 
 alias my_branches='gb  | grep nwhyte | sed "s/^\*//" | sed "s/^\ *//"';
 alias my_upstreams='git branch -r | grep nwhyte | sed "s/^*//" | sed "s/\ *origin\///" | sort'
@@ -44,23 +33,28 @@ be_owner() {
   cat $FILE;
 }
 
-
-gpc() {
-  if [ $# -ne 3 ]; then
-        echo "Usage: $0 <base-branch> <branch> <commit to pick>";
-        return 1
-    fi;
-
-    git checkout -b $2;
-    git reset --keep $1;
-    git cherry-pick $3;
-    ggpush;
-}
-
 export GOPATH=~/go
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH=$PATH:~/go/bin:~/work/scripts:~/work/scripts/bin:~/work/scripts/productivity-hacks
 signon
 
 
 . /Users/nickw/.nix-profile/etc/profile.d/nix.sh
 
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /Users/nickw/work/canva/web/tools/renderer/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/nickw/work/canva/web/tools/renderer/node_modules/tabtab/.completions/serverless.zsh
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /Users/nickw/work/canva/web/tools/renderer/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/nickw/work/canva/web/tools/renderer/node_modules/tabtab/.completions/sls.zsh
+
+prompt_context() {
+  # Only show the first char of the username
+  prompt_segment black white "%(!.%{%F{yellow}%}.)$USER[1]@%m"
+}
+
+git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "(%{$fg_bold[black]%}${ref#refs/heads/}%{${reset_color}%})"
+}
